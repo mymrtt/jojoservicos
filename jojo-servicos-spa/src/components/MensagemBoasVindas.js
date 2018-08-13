e9135803f07dc9beaf915baf8ce9be1464a4e324
CissaNina <alicegpt2010@gmail.com>
2018-08-13 10:20:00 -0300

    alterando footer desktop

--- jojo-servicos-spa/src/components/MensagemBoasVindas.js
+++ jojo-servicos-spa/src/components/MensagemBoasVindas.js
@@ -1,22 +1,24 @@
 import React from 'react';
 
 const Mensagem = () =>
-  <div className="caixa_inicial-boasvindas">
-    <h1 className="caixa_inicial-titulo">
-      Fique tranquilo,<br /> 
-      você encontrou a Jojô
-    </h1>
-    <h1 className="caixa_inicial-tituloMobile">
-      Fique tranquilo,<br /> 
-      você encontrou a Jojô
-    </h1>
-    <p className="caixa_inicial-descrição">
-    Vou ajudar você a superar os seus problemas burocráticos!
-
-    </p>
-    <p className="caixa_inicial-descriçãoMobile">
+  <div className="caixa_inicial-boasvindas-1">
+    <div className="teste">
+      <h1 className="caixa_inicial-titulo">
+        Fique tranquilo,<br /> 
+        você encontrou a Jojô
+      </h1>
+      <h1 className="caixa_inicial-tituloMobile">
+        Fique tranquilo,<br /> 
+        você encontrou a Jojô
+      </h1>
+      <p className="caixa_inicial-descrição">
       Vou ajudar você a superar os seus problemas burocráticos!
-    </p>	
+
+      </p>
+      <p className="caixa_inicial-descriçãoMobile">
+        Vou ajudar você a superar os seus problemas burocráticos!
+      </p>
+    </div>	
     <div className="caixa_inicial-indicacao">
      <img className="imgdeCompartilhar" src="share.png" />
       <p>Indique para um amigo</p>
