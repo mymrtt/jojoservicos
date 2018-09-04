eb8df2ff195f6c161cc9d8b1fd00e03eeb861074
CissaNina <alicegpt2010@gmail.com>
2018-09-04 15:15:11 -0300

    botoes compartilhar

--- jojo-servicos-spa/src/components/MensagemBoasVindas.js
+++ jojo-servicos-spa/src/components/MensagemBoasVindas.js
@@ -18,16 +18,33 @@ const Mensagem = () =>
         Vou ajudar você a superar os seus problemas burocráticos!
       </p>
     </div>	
-    <button className="caixa_inicial-indicacao">
-     <img className="imgdeCompartilhar" src="share.png" />
+    <div className="caixa_inicial-indicacao" id="botaoOpen" >
+     {/* <img className="imgdeCompartilhar" src="share.png" /> */}
       <p>Indique para um amigo</p>
-    </button>
-    <div className="caixa_inicial--compartilhar">
+    </div>
+    <div className="caixa_inicial--compartilhar" id="caixaIcones">
       <div className="compartilhar_icone">
+        <iframe src="https://www.facebook.com/plugins/share_button.php?href=https%3A%2F%2Fjojo-servicos.firebaseapp.com%2F&layout=button&size=small&mobile_iframe=true&width=97&height=20&appId" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>      
       </div>
-      <div className="compartilhar_icone"></div>
+      <div className="compartilhar_icone">
+        <a class="twitter-share-button"
+        href="https://twitter.com/intent/tweet?text=Hello%20world"
+        data-size="large" target="_blank">
+          <img className="icone-twitter" src="twitter-logo.svg" />      
+        Tweetar</a>
+      </div>
       <div className="compartilhar_icone"></div>
     </div>
   </div>;
 
+  //  var botaoOpen = document.querySelector("#botaoOpen");  
+  //  var caixaIcones = document.querySelector("#caixaIcones");  
+  //  caixaIcones.style.display = "none";  
+  //  botaoOpen.addEventListener("click", function(){  
+  //      if(caixaIcones.style.display == "block") {  
+  //          caixaIcones.style.display = "none";  
+  //      } else {  
+  //          caixaIcones.style.display = "block";  
+  //      }  
+  //  }, false);  
 export default Mensagem;
