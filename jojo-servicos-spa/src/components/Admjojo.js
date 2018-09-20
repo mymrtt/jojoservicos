087250cf258e33dbeb19f9ea4ce1d3e6b56120bb
mymrtt <mymrtt@gmail.com>
2018-09-20 16:05:09 -0300

    update

--- jojo-servicos-spa/src/components/Admjojo.js
+++ jojo-servicos-spa/src/components/Admjojo.js
@@ -9,23 +9,69 @@ const Container = styled.div`
   background: deepSkyBlue;
 `;
 
+const Section = styled.section`
+
+`;
+
+const Titulo_topo = styled.h1`
+  color: #fff;
+  font-size: 40px;
+  font-family: Helvetica, Bold;
+
+`;
+
+const Container_conteudo = styled.div`
+  width: 400px;
+  height: 300px;
+  background: #F7F7F7;
+  border-radius: 55px;
+`;
+
+const Logo_jojo = styled.img`
+
+`;
+
+const Container_input = styled.div`
+  width: 300px;
+  height: 100px;
+  border: 1px solid #3358A4;
+  border-radius: 22px;
+`;
+
+const Input_login = styled.input`
+
+`;
+
+const Input_senha = styled.input`
+
+`;
+
+const Container_entrar = styled.div`
+
+`;
+
+const Button_entrar = styled.button`
+  color: #fff;
+  background: linear-gradient(0deg,#69B42E,#569A21);
+`;
+
 const Admjojo = () =>
-  <Container className="Admjojo">
+  <Container>
     <Header />
-    <section className="">
-      <h1>Central de visualização de pedidos da Jojô</h1>
-      <div>
-        <img src="./logo-jojo.svg" alt="logo jojo" />
-        <div>
-          <input type="text" placeholder="Login"  />
-          <input type="text" placeholder="Senha" />
-        </div>
-        <div>
+    <Section>
+      <Titulo_topo>Central de visualização de pedidos da Jojô</Titulo_topo>
+      <Container_conteudo>
+        <Logo_jojo src="./logo-jojo.svg" alt="logo jojo" />
+        <Container_input>
+          <Input_login type="text" placeholder="Login" />
+          <Input_senha type="text" placeholder="Senha" />
+        </Container_input>
+        <Container_entrar>
           <a href=" ">Esqueceu a senha?</a>
-          <button>Entrar</button>
-        </div>
-      </div>
-    </section>
+          <Button_entrar>Entrar</Button_entrar>
+        </Container_entrar>
+      </Container_conteudo>
+    </Section>
     <Footer />
   </Container>
 
