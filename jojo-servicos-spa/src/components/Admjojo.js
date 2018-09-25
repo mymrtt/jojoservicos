253c63e08e8cbde2addc53374d0e9bd7a0f3c475
mymrtt <mymrtt@gmail.com>
2018-09-25 10:04:24 -0300

    update

--- jojo-servicos-spa/src/components/Admjojo.js
+++ jojo-servicos-spa/src/components/Admjojo.js
@@ -16,6 +16,13 @@ const Section = styled.section`
   border-top: 3px solid #69B42E;
   border-bottom: 3px solid #69B42E;
   background: linear-gradient(#2E7BB4,#306BAD, #3260A8, #3358A4);
+  @media(max-width: 640px){
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    border-top: 4px solid #69B42E;
+    border-bottom: 4px solid #69B42E;
+  }
 `;
 
 const Titulo_topo = styled.h1`
@@ -24,43 +31,61 @@ const Titulo_topo = styled.h1`
   color: #fff;
   font-size: 40px;
   font-family: Helvetica, Bold;
+  @media(max-width: 640px){
+    width: 332px;
+    font-size: 27px;
+  }
 `;
 
-const Container_content = styled.div`
+const Container_login = styled.div`
   width: 400px;
-  height: 350px;
+  height: 370px;
   display: flex;
+  align-items: center;
   flex-direction: row;
   background: #F7F7F7;
   border-radius: 55px;
+  @media(max-width: 640px){
+    /* width: 339px;
+    height: 400px; */
+    width: 90%;
+    height: 75%;
+  }
 `;
 
-const Container_content_items = styled.div`
+const Container_login_items = styled.div`
   width: 100%;
   height: 320px;
   display: flex;
-  flex-direction: column;
   justify-content: center;
   align-items: center;
+  flex-direction: column;
 `;
 
 const Logo_jojo = styled.img`
   width: 170px;
-  height: 100px;
+  height: 108px;
+  @media(max-width: 640px){
+    /* width: 200px; */
+    width: 60%;
+  }
 `;
 
 const Container_input = styled.div`
-  width: 300px;
-  height: 125px;
+  width: 315px;
+  height: 135px;
   display: flex;
-  flex-direction: column;
   justify-content: space-evenly;
   align-items: center;
+  flex-direction: column;
+  @media(max-width: 640px){
+    width: 100%;
+  }
 `;
 
 const LoginFormInput = styled.input`
-  width: 290px;
-  height: 35px;
+  width: 310px;
+  height: 36px;
   color: #7F7F7F;
   font-size: 15px;
   padding-left: 15px;
@@ -68,33 +93,53 @@ const LoginFormInput = styled.input`
   border-radius: 22px;
   outline: none;
   opacity: 0.7;
+  @media(max-width: 640px){
+    /* width: 300px;
+    height: 44px; */
+    width: 90%;
+    height: 30%;
+    border: 2px solid #3358A4;
+  }
 `;
 
 const Container_entrar = styled.div`
-  width: 325px;
+  width: 345px;
   height: 50px;
   display: flex;
   justify-content: flex-end;
   align-items: flex-end;
   font-family: Museo;
   font-size: 15px;
+  @media(max-width: 640px){
+    /* width: 320px; */
+    width: 100%;
+  }
 `;
 
 const Container_entrar_items = styled.div`
-  width: 285px;
+  width: 295px;
   display: flex;
   justify-content: space-evenly;
   align-items: center;
+  @media(max-width: 640px){
+    width: 94%;
+  }
 `;
 
 const Button_entrar = styled.button`
-  width: 130px;
+  width: 135px;
   height: 35px;
   color: #fff;
   border: #000;
   border-radius: 18px;
   outline: none;
   background: linear-gradient(0deg,#69B42E,#569A21);
+  @media(max-width: 640px){
+    /* width: 150px;
+    height: 40px; */
+    width: 45%;
+    height: 6vh;
+  }
 `;
 
 const Admjojo = () =>
@@ -102,8 +147,8 @@ const Admjojo = () =>
     <Header />
     <Section>
       <Titulo_topo>Central de visualização de pedidos da Jojô</Titulo_topo>
-      <Container_content>
-        <Container_content_items>
+      <Container_login>
+        <Container_login_items>
           <Logo_jojo src="./logo-jojo.svg" alt="logo jojo" />
           <Container_input>
             <LoginFormInput type="email" name="email" placeholder="Log in" />
@@ -115,18 +160,11 @@ const Admjojo = () =>
               <Button_entrar>Entrar</Button_entrar>
             </Container_entrar_items>
           </Container_entrar>
-        </Container_content_items>
-      </Container_content>
+        </Container_login_items>
+      </Container_login>
     </Section>
     <Footer />
   </Container>
-
-
-
-
-
-
-
 
 
 export default Admjojo;
